b704be27f22cc1f8c8442ed0182a3781f1e5e102
Lucifer-Saksham <saksham.miglani.official@gmail.com>
2025-05-06 11:12:44 +0530

    Initial commit with project setup

---
+++ src/app/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to TimeNest</h1>
+      <p>Your simple time management app.</p>
+      <ul>
+        <li>Pomodoro Timer</li>
+        <li>To-Do List</li>
+        <li>Daily Planner</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
